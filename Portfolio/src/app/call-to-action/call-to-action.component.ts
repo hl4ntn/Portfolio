a2e89aed9ad3ce31e64110ae53d0f35e5778b5e7
hl4ntn <184815703+hl4ntn@users.noreply.github.com>
2026-01-08 14:21:16 +0100

    workedn on input fields on contact form

--- Portfolio/src/app/call-to-action/call-to-action.component.ts
+++ Portfolio/src/app/call-to-action/call-to-action.component.ts
@@ -2,17 +2,27 @@ import { AfterViewInit, Component } from '@angular/core';
 import { DotsComponent } from '../dots/dots.component';
 import { NavbarComponent1 } from '../navbar/navbar.component';
 import { PagesService } from '../pages.service';
+import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-call-to-action',
   standalone: true,
-  imports: [NavbarComponent1, DotsComponent],
+  imports: [NavbarComponent1, DotsComponent, FormsModule],
   templateUrl: './call-to-action.component.html',
   styleUrl: './call-to-action.component.scss'
 })
 export class CallToActionComponent implements AfterViewInit {
 isActive = false;
+inputName = "Your name goes here";
+inputEmail = "youremail@email.com";
+inputMessage = "Hello Hannah, I am interested in...";
+currentData = {
+  name: '',
+  email: '',
+  message: '',
+};
 
   constructor(public pageService: PagesService) {  
       }
@@ -37,6 +47,35 @@ isActive = false;
 
   toggle() {
     this.isActive = !this.isActive;
+  }
+
+  sendInputData(){
+    
+    if (this.inputName != "Your name goes here"){
+      console.log(this.inputName);
+    } else{
+      this.showAlert('inputName');
+    }
+    if (this.inputEmail != "youremail@email.com") {
+      console.log(this.inputEmail);
+    } else{
+      this.showAlert('inputEmail');
+    }
+    if(this.inputMessage != "Hello Hannah, I am interested in..."){
+      console.log(this.inputMessage);
+    } else{
+      this.showAlert('inputMessage');
+    }
+    
+  }
+
+  emptyInput(input:any){
+     (this as any)[input] = '';
+    
+  }
+
+  showAlert(input: any){
+    (this as any)[input] = "Oops! it seems your name is missing";
   }
   
 }
