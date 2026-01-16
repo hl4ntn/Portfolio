bb653c027a1c0ee2cbaab24ac845c03f2d7c7b28
hl4ntn <184815703+hl4ntn@users.noreply.github.com>
2026-01-16 16:00:38 +0100

    tried php mail

--- Portfolio/src/app/call-to-action/call-to-action.component.ts
+++ Portfolio/src/app/call-to-action/call-to-action.component.ts
@@ -1,9 +1,10 @@
-import { AfterViewInit, ViewChild, Component } from '@angular/core';
+import { AfterViewInit, ViewChild, Component, inject } from '@angular/core';
 import { DotsComponent } from '../dots/dots.component';
 import { NavbarComponent1 } from '../navbar/navbar.component';
 import { PagesService } from '../pages.service';
 import { FormsModule, NgForm, NgModel } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 
 
@@ -15,6 +16,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './call-to-action.component.scss'
 })
 export class CallToActionComponent implements AfterViewInit {
+
+  http = inject(HttpClient);
+
   @ViewChild('emailCtrl') emailCtrl!: NgModel;
 isActive = false;
 missingNameData = false;
@@ -26,6 +30,17 @@ currentData = {
   email: '',
   message: '',
 };
+
+  post = {
+    endPoint: 'https://hannah-anton.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
 
   constructor(public pageService: PagesService) {  
       }
@@ -58,12 +73,24 @@ currentData = {
   }
 
   onSubmit(ngForm: NgForm) {
-    if(ngForm.valid && ngForm.submitted){
-      console.log(this.currentData, 'ich wurde ausgeführt');
+       if (ngForm.submitted && ngForm.form.valid) {
+        console.log(this.currentData, 'ich wurde ausgeführt');
       this.missingNameData = false;
       this.missingEmailData = false;
       this.missingMessageData = false;
-      ngForm.resetForm();
+      this.http.post(this.post.endPoint, this.post.body(this.currentData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+
+        ngForm.resetForm();
       this.isActive = false;
     } else {
       this.showAlert();
