93e9d45f2c513ca53085438c919a0d3ba7ed4415
hl4ntn <184815703+hl4ntn@users.noreply.github.com>
2026-02-06 19:53:28 +0100

    finished english version and started burger menu

--- Portfolio/src/app/legal-notice/legal-notice.component.ts
+++ Portfolio/src/app/legal-notice/legal-notice.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PagesService } from '../pages.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
@@ -13,5 +14,25 @@ export class LegalNoticeComponent {
   constructor(public pageService: PagesService){
 
   }
+
+   toggleLanguage(){
+      if(this.pageService.currentLanguage == "en"){
+        this.pageService.currentLanguage = "de"
+      } else{
+        this.pageService.currentLanguage = "en"
+      }
+    }
+
+    goBack() {
+  if(this.pageService.currentPage == 5){
+        window.history.back();
+      } else{
+        window.history.go(-2);  
+      }
+}
+
+switchCurrentPage(){
+  this.pageService.currentPage = 0;
+}
 
 }
--- Portfolio/src/app/privacy-policy/privacy-policy.component.ts
+++ Portfolio/src/app/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PagesService } from '../pages.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
 
+  constructor(public pageService: PagesService){
+  
+    }
+  toggleLanguage(){
+      if(this.pageService.currentLanguage == "en"){
+        this.pageService.currentLanguage = "de"
+      } else{
+        this.pageService.currentLanguage = "en"
+      }
+    }
+
+    goBack() {
+      if(this.pageService.currentPage == 5){
+        window.history.back();
+      } else{
+        window.history.go(-2);  
+      }
+
+  console.log(this.pageService.currentPage);
+}
+switchCurrentPage(){
+  this.pageService.currentPage = 0;
+}
 }
